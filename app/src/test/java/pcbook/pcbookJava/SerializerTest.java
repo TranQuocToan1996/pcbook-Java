cb82de8f8253cd46934ea570781e0741bf170fb2
TranQuocToan1996 <tranquoctoan.ce@gmail.com>
2023-01-17 18:16:55 +0700

    Serialize protobuf message, unit test

---
+++ app/src/test/java/pcbook/pcbookJava/SerializerTest.java
@@ -0,0 +1,25 @@
+package pcbook.pcbookJava;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import pcbookJava.Laptop;
+import sample.Generator;
+import serializer.Serializer;
+
+public class SerializerTest {
+    @Test
+    public void writeAndReadBinaryFile() throws IOException {
+        final String binaryFile = "../laptop.bin";
+        Laptop laptop1 = new Generator().NewLaptop();
+
+        Serializer serializer = new Serializer();
+        serializer.WriteBinaryFile(laptop1, binaryFile);
+
+        Laptop laptop2 = serializer.ReadBinaryFile(binaryFile);
+        Assert.assertEquals(laptop1, laptop2);
+    }
+
+}
